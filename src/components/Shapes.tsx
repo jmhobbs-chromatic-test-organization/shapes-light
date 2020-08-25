574fd28a1648a730613141389674aa8ac64635ec
Gert Hengeveld <info@ghengeveld.nl>
2020-08-25 09:51:32 +0200

    Restore colors.

--- src/components/Shapes.tsx
+++ src/components/Shapes.tsx
@@ -14,7 +14,7 @@ const Shape = styled.svg<Props>(({ color }) => ({
 }));
 
 export const Donut = (props: Props) => (
-  <Shape viewBox="0 0 90 90" xmlns="http://www.w3.org/2000/svg" color="orange" {...props}>
+  <Shape viewBox="0 0 90 90" xmlns="http://www.w3.org/2000/svg" color="#37D5D3" {...props}>
     <path d="M45 0c24.853 0 45 20.147 45 45S69.853 90 45 90 0 69.853 0 45 20.147 0 45 0zm.5 27C35.283 27 27 35.283 27 45.5S35.283 64 45.5 64 64 55.717 64 45.5 55.717 27 45.5 27z" />
   </Shape>
 );
@@ -26,7 +26,7 @@ export const L = (props: Props) => (
 );
 
 export const Slice = (props: Props) => (
-  <Shape viewBox="0 0 92 92" xmlns="http://www.w3.org/2000/svg" color="teal" {...props}>
+  <Shape viewBox="0 0 92 92" xmlns="http://www.w3.org/2000/svg" color="#FF4785" {...props}>
     <path d="M92 89V3c0-3-2.056-3-3-3C39.294 0 0 39.294 0 89c0 0 0 3 3 3h86a3 3 0 003-3z" />
   </Shape>
 );
